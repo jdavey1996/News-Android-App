9d08e6a2e39cba59cf4a46a45bb9f0e9b4141f0e
Josh Davey <jdavey1996@gmail.com>
2016-08-04 21:56:27 +0100

    Permissions & Network providers
    Called a line of code to request permission to use the devices location,
    through a dialog box.
    Created a method to check if the user grants the app permission to use
    its location settings.
    Created a method to check if a location provider is enabled. GPS or
    Network.

--- app/src/main/java/com/josh_davey/news_app/Fragment1.java
+++ app/src/main/java/com/josh_davey/news_app/Fragment1.java
@@ -1,18 +1,25 @@
 package com.josh_davey.news_app;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends Fragment{
     public Fragment1() {
         // Required empty public constructor
     }
+
+    //Variables
+    LocationManager locationManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,15 +31,22 @@ public class Fragment1 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Requesting permission to access device location.
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
     }
+
+
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
 
-        //When fragment 1 is visible to users, log the GPS and network status.
         if (isVisibleToUser) {
-            LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+            locationManager= (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+            //Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            //Location locationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
             Boolean isGPSEnabled;
             Boolean isNetworkEnabled;
 
@@ -40,8 +54,32 @@ public class Fragment1 extends Fragment {
             isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
             Log.i("GPS Status", String.valueOf(isGPSEnabled));
-
             Log.i("Network Status", String.valueOf(isNetworkEnabled));
         }
     }
+
+    public boolean checkPermissionGranted()
+    {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public boolean checkLocationProviderEnabled()
+    {
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)== true || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
+
