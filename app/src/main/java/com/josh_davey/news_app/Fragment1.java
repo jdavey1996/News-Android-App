8d05b3c7f999b7655eedf53d5bd2a332a129fb9b
Josh Davey <jdavey1996@gmail.com>
2016-08-18 22:08:36 +0100

    Location Permission Error catching
    Updated the way permissions are requested and checked. The app now
    checks onStart if location permissions are granted, if they are,
    location updates are requested and data is loaded. If not the app
    requests them. The onRequestPermissionsResults method has been
    implemented to check if the user granted the permisions when asked, if
    so data is loaded.
    When swipe refreshing the data, the user is prompted to grant
    permissions if they're not already. The same process as the onStart
    implements.

--- app/src/main/java/com/josh_davey/news_app/Fragment1.java
+++ app/src/main/java/com/josh_davey/news_app/Fragment1.java
@@ -8,7 +8,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -30,26 +29,45 @@ public class Fragment1 extends Fragment{
     LocationManager mLocationManager;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        //Requesting permission to access device location.
-        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
-
+    public void onStart() {
+        super.onStart();
+        /*Everytime the app is started (including closed and reopened), it checks if permissions for location aree granted.
+          If so it requests location updates and runs the load data method.
+          If not, it requests permissions from the user.*/
         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationManager = (LocationManager) getContext().getSystemService(getContext().LOCATION_SERVICE);
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+            loadData();
         }
-
+        else
+        {
+            //Requesting permission to access device location.
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
     }
 
-
+    //Runs when the user responds to permission requests.
     @Override
-    public void onStart() {
-        super.onStart();
-        loadData();
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode)
+        {
+            //Tasks to execute depending on the response from a request to access location data.
+            case 1:
+                //If the user grants the location permission, request location updates and load data. Else _____ **add a text field that displays**
+                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mLocationManager = (LocationManager) getContext().getSystemService(getContext().LOCATION_SERVICE);
+                    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+                    loadData();
+                    Log.i("Location permissions","Granted");
+                }
+                else
+                {
+                    Log.i("Location permissions","Not granted");
+                }
+        }
     }
 
+    //Listener for location updates.
     private final LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(final Location location) {
@@ -73,11 +91,11 @@ public class Fragment1 extends Fragment{
     };
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment1, container, false);
 
+        //Initialises swipe refresh layout 1 for fragment1.
         sw = (SwipeRefreshLayout)view.findViewById(R.id.refreshLayout1);
 
         //Reloads data on swipe down.
@@ -85,39 +103,51 @@ public class Fragment1 extends Fragment{
                 new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
                     public void onRefresh() {
-                        loadData();
+                        //If permission is granted, load data. Else request permission.
+                        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                            loadData();
+                        }
+                        else
+                        {
+                            //Cancel refresh animation.
+                            sw.setRefreshing(false);
+                            //Requesting permission to access device location.
+                            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+                        }
                     }
                 }
         );
         return view;
     }
 
-
-
-
     public void loadData()
     {
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
+        try {
+            //Gets the last known location.
             Location loc = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
+            //Runs the last known location through the getCity method to return a city string.
             String latestCity = getCity(loc);
+            //If the city returned is null, error.
+            //If not, execute the getArticles asynctask, passing the city string as an argument to get the correct data.
             if (latestCity.equals("null")) {
-                Log.i("Loc", "err");
+                Log.i("Location", "err");
                 Toast.makeText(getContext(), "ERROR", Toast.LENGTH_SHORT).show();
                 sw.setRefreshing(false);
-            }
-            else
-            {
-                Log.i("lastest locc", latestCity);
+            } else {
+                Log.i("Latest Location", latestCity);
                 Toast.makeText(getContext(), latestCity, Toast.LENGTH_SHORT).show();
-                GetArticles getData = new GetArticles(getContext(),getActivity(),this);
+                GetArticles getData = new GetArticles(getContext(), getActivity(), this);
                 getData.execute(latestCity);
             }
         }
+        catch (SecurityException e)
+        {
+            Log.i("loadData()exception", e.toString());
+        }
     }
 
-
+    //Uses a location, running ti through a geocoder to get the data extracted. This then gets the city from that data and returns it.
     public String getCity(Location location)
     {
         Geocoder geo = new Geocoder(getActivity().getApplicationContext(), Locale.getDefault());
@@ -131,10 +161,8 @@ public class Fragment1 extends Fragment{
         }
         catch (Exception e)
         {
-
+            Log.i("getCity()exception", e.toString());
         }
         return city;
     }
 }
-
-
