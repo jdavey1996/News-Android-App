b46712e53b27314a46a8596832bed06061367818
Josh Davey <jdavey1996@gmail.com>
2016-10-24 00:06:04 +0100

    Implemented GoogleAPI Location
    Updated the method of acquiring location to use google api.

--- app/src/main/java/com/josh_davey/news_app/Fragment1.java
+++ app/src/main/java/com/josh_davey/news_app/Fragment1.java
@@ -17,42 +17,68 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+
 import java.util.List;
 import java.util.Locale;
 
-public class Fragment1 extends Fragment{
-    public Fragment1() {
-        // Required empty public constructor
-    }
+public class Fragment1 extends Fragment implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
+
 
     SwipeRefreshLayout sw;
-    LocationManager mLocationManager;
+    GoogleApiClient googleApiClient;
+    LocationRequest locationRequest;
+    GoogleApiAvailability availabilityInstance = GoogleApiAvailability.getInstance();
+    String currentLocation = null;
+    LocationSettingsRequest locationSettingsRequest;
 
     @Override
     public void onStart() {
         super.onStart();
-        /*Everytime the app is started (including closed and reopened), it checks if permissions for location are granted.
-          If so it requests location updates and runs the load data method.
-          If not, it requests permissions from the user.*/
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mLocationManager = (LocationManager) getContext().getSystemService(getContext().LOCATION_SERVICE);
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
-            loadData();
-        } else {
-            //Requesting permission to access device location.
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
+        startGoogleApi();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        //When the app is paused (eg, returning to home screen without closing), location updates are removed to save battery.
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mLocationManager.removeUpdates(mLocationListener);
+        //If the googleApiClient is connected, stop location updates and disconnect (Save battery).
+        if (googleApiClient.isConnected())
+        {
+            stopLocationUpdates();
+            googleApiClient.disconnect();
         }
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //If the googleApiClient is connected, stop location updates and disconnect (Save battery).
+        if (googleApiClient.isConnected()) {
+            stopLocationUpdates();
+            googleApiClient.disconnect();
+        }
+    }
+
+
+    public void startGoogleApi()
+    {
+        //Check if the googleApiClient has already been built. If so check run googlePlayServicesAvailability method to check availability.
+        if (googleApiClient!= null)
+        {
+            googlePlayServicesAvailability();
+        }
+        //If not, build the googleApiClient, if successful (returned true), then run googlePlayServicesAvailability method to check availability.
+        else {
+            if (buildGoogleApiClient() == true) {
+                googlePlayServicesAvailability();
+            }
+        }
+    }
     //Runs when the user responds to permission requests.
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -62,10 +88,10 @@ public class Fragment1 extends Fragment{
             case 1:
                 //If the user grants the location permission, request location updates and load data.
                 if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    mLocationManager = (LocationManager) getContext().getSystemService(getContext().LOCATION_SERVICE);
-                    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
-                    loadData();
+
                     Log.i("Location permissions","Granted");
+
+                    startGoogleApi();
                 }
                 else
                 {
@@ -76,28 +102,6 @@ public class Fragment1 extends Fragment{
         }
     }
 
-    //Listener for location updates.
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -114,7 +118,7 @@ public class Fragment1 extends Fragment{
                     public void onRefresh() {
                         //If permission is granted, load data. Else request permission.
                         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                            loadData();
+                            //Reload data.
                         }
                         else
                         {
@@ -129,51 +133,124 @@ public class Fragment1 extends Fragment{
         return view;
     }
 
-    public void loadData()
-    {
+    //Build google api client for location services api. Return true if successful.
+    protected synchronized boolean buildGoogleApiClient() {
         try {
-
-            //Gets the last known location.
-            Location loc = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-            //Runs the last known location through the getCity method to return a city string.
-            String latestCity = getCity(loc);
-            //If the city returned is null, error.
-            //If not, execute the getArticles asynctask, passing the city string as an argument to get the correct data.
-            if (latestCity == "null") {
-                sw.setRefreshing(false);
-                Toast.makeText(getContext(), "Unable to acquire your location, check your settings and try again later.", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getContext(), latestCity, Toast.LENGTH_SHORT).show();
-                GetArticles getData = new GetArticles(getContext(), getActivity(), this);
-                getData.execute(latestCity);
-            }
-        }
-        catch (SecurityException e)
+            googleApiClient = new GoogleApiClient.Builder(getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            return true;
+        }catch (Exception e)
         {
+            return false;
         }
     }
 
-    //Uses a location, running it through a geocoder to get the information behind the lat and long values.
-    //This then gets the city from that information and returns it.
-    public String getCity(Location location)
+    public void googlePlayServicesAvailability()
+    {
+        //Gets google play services availability.
+        int resultCode = availabilityInstance.isGooglePlayServicesAvailable(getContext());
+
+        if (resultCode == ConnectionResult.SUCCESS)
+        {
+            //If the result code is success, check if the api client is connecting or connected. If it isn't then connect.
+            if (!googleApiClient.isConnecting()== true && !googleApiClient.isConnected() == true) {
+                googleApiClient.connect();
+            }
+        }
+        else
+        {
+            //If the result code isn't success, display resolvable error dialog if it is a resolvable case.
+            if (availabilityInstance.isUserResolvableError(resultCode)){
+                availabilityInstance.getErrorDialog(getActivity(), resultCode, 1).show();
+            }
+        }
+    }
+
+    //Defines details of location data to be requested.
+    protected void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(2000);
+        locationRequest.setFastestInterval(50);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    public void startLocationUpdates()
+    {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
+        else {
+            //Requesting permission to access device location.
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+    public void stopLocationUpdates()
+    {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+    }
+
+
+
+    //Callback and listener methods:
+    @Override
+    public void onLocationChanged(Location location) {
+       // Toast.makeText(getContext(), "", Toast.LENGTH_SHORT).show();
+        getCity(location);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(getContext(), "Failed to connect", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(getContext(), "Suspended connection", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(getContext(), "Connected", Toast.LENGTH_SHORT).show();
+        createLocationRequest();
+        startLocationUpdates();
+    }
+
+
+    public void getCity(Location location)
     {
         try {
             Geocoder geo = new Geocoder(getActivity().getApplicationContext(), Locale.getDefault());
             List<Address> data;
             String city = "null";
-            try {
-                data = geo.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                if (data.size() > 0) {
-                    city = data.get(0).getLocality();
+            data = geo.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (data.size() > 0) {
+                city = data.get(0).getLocality();
+                if (city.equals(currentLocation))
+                {
+                    //No location change.
+                    Toast.makeText(getContext(), "No location change", Toast.LENGTH_SHORT).show();
                 }
-            } catch (Exception e) {
+                else if (city.equals("null"))
+                {
+                    //No city data.
+                    Toast.makeText(getContext(), "No city data", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //Location has changed. Load data again.
+                    currentLocation = city;
+                    Toast.makeText(getContext(), "Location changed", Toast.LENGTH_SHORT).show();
+                }
             }
-            return city;
         }
         catch (Exception e)
         {
-            return "null";
         }
     }
+
+
 }
